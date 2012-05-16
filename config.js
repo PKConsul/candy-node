36cca97322aa973fb75d8d92461b81be71c7d767
Michael Weibel <michael.weibel@gmail.com>
2012-05-16 08:20:37 +0200

    Config.js for openshift

---
+++ config.js
@@ -0,0 +1,22 @@
+var config = {
+	// Candy settings
+	// See http://candy-chat.github.com/candy/#configuration
+	candy: {
+		core: { debug: false, autojoin: ['candy_demo@conference.swissjabber.ch'] },
+		view: { resources: 'candy/res/' },
+		// Args passed to Candy.Core.connect();
+		connect: ['demo.swissjabber.ch']
+	},
+	// HTTP-Bind settings
+	http_bind: {
+		host: 'localhost',
+		port: 5280,
+		path: '/http-bind/'
+	},
+	// App setting
+	app: {
+		port: 80
+	}
+};
+
+module.exports = config;
